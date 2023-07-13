7298b27b92086c99589f91cdd373f01264ee4da7
onkelhoy <onkelhoy@gmail.com>
2023-07-13 12:02:45 +0200

    wip: radio

--- packages/atoms/radio/src/component.ts
+++ packages/atoms/radio/src/component.ts
@@ -2,42 +2,37 @@
 import { html, property } from "@circular-tools/utils";
 
 // templates
-import { BaseTemplate } from "@circular-templates/base";
+import { FieldTemplate } from "@circular-templates/field";
 
 // local 
 import { style } from "./style";
 import { Foo, ClickEvent } from "./types";
 
-export class Radio extends BaseTemplate {
+export class Radio extends FieldTemplate {
     static style = style;
 
-    @property() foo:Foo = "bar";
-    @property({ type: Number }) bajs?:number;
-    @property({ type: Boolean }) fooLaa:boolean = true;
+    // @property({ rerender: false }) checked
 
-    // event handlers
-    private handleMainClick() {
-        this.dispatchEvent(new CustomEvent<ClickEvent>("main-click", { detail: { foo: this.foo } }));
+    constructor() {
+        super();
+
+        this.addEventListener("click", this.handleclick);
+    }
+
+    // event handlers 
+    private handleclick = () => {
+
     }
 
     render() {
-        return html`
-            <header part="header">
-                <slot name="header">
-                    <h1>llama drama trauma</h1>
-                </slot>
-            </header>
-            <main onclick=${this.handleMainClick}>
-                <slot>
-                    <p>Why did the llama go to therapy? Because it had a lot of spitting issues!</p>
-                </slot>
-            </main>
-            <footer part="footer">
-                <slot name="footer">
-                    <p>Why did the llama enter the door? To attend the llamazing party inside!</p>
-                </slot>
-            </footer>
-        `
+        return super.render(html`
+            <div class="radio">
+                <slot name="prefix"></slot>
+                <span part="indicator"></span>
+                <slot name="suffix"></slot>
+            </div>
+            <input type="radio" hidden readonly />
+        `)
     }
 }
 
