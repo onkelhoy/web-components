467bc1067aacb7739c99d62cafc7b0e37c720a3c
Henry <onkelhoy@gmail.com>
2024-09-28 16:28:41 +0700

    add: template unit test improved with events

--- bin/create/component/template/default/tests/unit.test.ts
+++ bin/create/component/template/default/tests/unit.test.ts
@@ -5,10 +5,65 @@ test.beforeEach(async ({ page }) => {
   await page.goto('PLACEHOLDER_NAME');
 });
 
+declare global {
+  interface Window {
+    EVENT_EMITTED: any;
+  }
+}
+
 test.describe("PLACEHOLDER_FULL_NAME unit tests", () => {
   test('available in DOM', async ({ page }) => {
     // Interact with your component and make assertions
     const component = await page.$('PLACEHOLDER_HTML_NAME');
     expect(component).not.toBeNull();
+  });
+});
+
+test.describe.skip("helpers", () => {
+  test("wait for event : enter press", async ({ page }) => {
+    const target = page.getByTestId("base-target");
+    await page.evaluate(() => {
+      window.EVENT_EMITTED = null;
+      const target = document.querySelector<HTMLElement>("*[data-testid='base-target']");
+      if (!target) return;
+
+      target.addEventListener("event-name", () => {
+        window.EVENT_EMITTED = true;
+      });
+
+      target.focus();
+      target.dispatchEvent(new KeyboardEvent("up", { key: "Enter" }));
+      // enter is tricky to capture so we make sure to dispatch inside evalute
+    });
+
+    await target.focus();
+    await target.press("Enter");
+
+    expect(await page.evaluate(() => window.EVENT_EMITTED)).toBeTruthy();
+  });
+
+  test("should submit a form", async ({ page }) => {
+    // NOTE this requires in main.js you have form.onsubmit = (e) => { e.preventDefault() }
+    // setting this in evaluate did not work.. (maybe with page.evalute)
+
+    await page.getByTestId("a").fill("hello");
+    await page.getByTestId("submit").click();
+
+    const results = await page.evaluate(() => {
+      const form = document.querySelector("form");
+      if (!form) return null;
+
+      const formdata = new FormData(form);
+      return {
+        a: formdata.get("a"),
+        b: formdata.get("b"),
+      }
+    });
+
+    expect(results).not.toBe(null);
+    expect(results).toMatchObject({
+      a: "hello",
+      b: "value",
+    });
   });
 });
