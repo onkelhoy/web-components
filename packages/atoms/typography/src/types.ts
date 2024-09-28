81d1e9b4282c67bdf3cbc3af4636488165faa140
Henry <onkelhoy@gmail.com>
2024-09-28 16:33:39 +0700

    add: typography p2 variant introduces

--- packages/atoms/typography/src/types.ts
+++ packages/atoms/typography/src/types.ts
@@ -1,5 +1,5 @@
 // DESIRED for clean and html friendly 
-export type Variant = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "caption" | "p" | "label";
+export type Variant = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "caption" | "p" | "p2" | "label";
 
 // adrian logic (but it has all the styles)
 // type CommonVariantNames = 'C' | 'T' | 'H' | 'copy' | 'title' | 'heading';
